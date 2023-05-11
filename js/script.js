2f45846311739967a9267073b8f446611933e2dd
Cosimo-Colucci <ccoluccilavoro@gmail.com>
2023-05-11 15:55:48 +0200

    created the 4 const + getelementbyid

--- js/script.js
+++ js/script.js
@@ -1,3 +1,6 @@
-const YourName ;
-const YourSurName ;
-const YourFavoariteColor ;
+const userName = prompt ('Insert your name');
+const userSurName= prompt('Insert your surname');
+const userFavoariteColor = prompt('Insert your favorite color');
+const randomPassword = userName + userSurName + userFavoariteColor + 21;
+
+document.getElementById('random-password').innerHTML = randomPassword;
