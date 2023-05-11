6c6c80b4d443882833244a45e8bd05ea62323225
Cosimo-Colucci <ccoluccilavoro@gmail.com>
2023-05-11 16:30:09 +0200

    bonus 1 with backtip

--- js/script.js
+++ js/script.js
@@ -1,6 +1,10 @@
 const userName = prompt ('Insert your name');
 const userSurName= prompt('Insert your surname');
 const userFavoariteColor = prompt('Insert your favorite color');
-const randomPassword = userName + userSurName + userFavoariteColor + 21;
+const randomPassword = 
+    `
+        ${userName}${userSurName}${userFavoariteColor}${21}
+    `
+;
 
 document.getElementById('random-password').innerHTML = randomPassword;
